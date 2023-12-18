6315db42ab2e3f2196b47997ab26199329841e3b
Moe <quangkhanh1711.work@gmail.com>
2023-12-19 05:16:59 +0700

    add lazyload, menu page

---
+++ src/lazyload.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+	if (window.IntersectionObserver) {
+		const lazyImageList = document.querySelectorAll("img[lazy-src]");
+		let observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting && entry.target.hasAttribute("lazy-src")) load(entry.target);
+			});
+		});
+
+		lazyImageList.forEach((lazyImage) => {
+			observer.observe(lazyImage);
+		});
+	} else console.error("- lazyload with IntersectionObserver is not supported");
+}
+
+function load(lazyImage) {
+	const src = lazyImage.getAttribute("lazy-src");
+	lazyImage.setAttribute("src", src);
+	lazyImage.removeAttribute("lazy-src");
+}
